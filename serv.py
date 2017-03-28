50e9c81d6d834b2fed5997b22ea6a3b7b2d257b1
Samyak Jain <samyak.jain2016a@vitstudent.ac.in>
2017-03-28 23:34:33 +0530

    Added class to check for tokens

--- serv.py
+++ serv.py
@@ -3,9 +3,13 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.options
+import tornado.auth
+import tornado.gen
+
+from oauth2client import client, crypt
 from tornado.options import define, options
 
-
+CLIENT_ID = "284523515321-t0ugfeortsj0008gm63gs64153nm5omf.apps.googleusercontent.com"
 define("port", default=8000, help="runs on the given port", type=int)
 
 
@@ -21,25 +25,44 @@ class ErrorHandler(tornado.web.ErrorHandler, IndexHandler):
     pass
 
 
-class AllTypes(IndexHandler):
+class AuthHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        self.token = self.get_argument('token')
+        try:
+            idinfo = client.verify_id_token(self.token, CLIENT_ID)
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise crypt.AppIdentityError('Wrong issuer.')
+
+        except crypt.AppIdentityError as e:
+            self.set_status(401)
+            self.write({"status_code": 401, "status_message": self._reason})
+
+    def post(self):
+        pass
+
+
+class AllTypes(IndexHandler, AuthHandler):
     def get(self):
         try:
+            #  Writes all the events as a JSON
             self.write({"status_code": 200, "status_message": self._reason, "list": hcom.h_com() + hcom.guide_conf() + hcom.vencity()})
         except:
             raise tornado.web.HTTPError(500)
 
 
-class Hacks(IndexHandler):
+class Hacks(IndexHandler, AuthHandler):
     def get(self):
         try:
+            #  Writes all the hackathons as a JSON
             self.write({"status_code": 200, "status_message": self._reason, "list": hcom.h_com() + hcom.vencity()})
         except:
             raise tornado.web.HTTPError(500)
 
 
-class Conf(IndexHandler):
+class Conf(IndexHandler, AuthHandler):
     def get(self):
         try:
+            # Writes all the conferences as a JSON
             self.write({"status_code": 200, "status_message": self._reason, "list": hcom.guide_conf()})
         except:
             raise tornado.web.HTTPError(500)
@@ -55,7 +78,8 @@ if __name__ == "__main__":
         handlers=[
             (r"/", AllTypes),
             (r"/hackathons", Hacks),
-            (r"/conferences", Conf)
+            (r"/conferences", Conf),
+            (r"/create", AuthHandler)
         ], **settings
     )
     http_server = tornado.httpserver.HTTPServer(app)
