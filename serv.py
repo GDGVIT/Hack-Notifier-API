78dea33d4a5cc212b490c10d38f76a0a21a7e91a
Samyak Jain <samyak.jain2016a@vitstudent.ac.in>
2017-09-24 23:07:30 +0530

    Removed auth

--- serv.py
+++ serv.py
@@ -1,5 +1,4 @@
 import hcom
-import os
 import tornado.auth
 import tornado.gen
 import tornado.httpserver
@@ -7,22 +6,10 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
-from oauth2client import client, crypt
-from pymongo import MongoClient
 from tornado.options import define, options
 
-CLIENT_ID = os.environ['CLIENT_ID']
 define("port", default=8000, help="runs on the given port", type=int)
 
-dbuser = os.environ['dbuser']
-dbpass = os.environ['dbpass']
-uri = r"mongodb://" + dbuser + r":" + dbpass + r"@ds163340.mlab.com:63340/users"
-
-mclient = MongoClient(uri,
-                      connectTimeoutMS=30000,
-                      socketTimeoutMS=None,
-                      socketKeepAlive=True)
-db = mclient.get_default_database()
 
 class IndexHandler(tornado.web.RequestHandler):
     def write_error(self, status_code, **kwargs):
@@ -36,43 +23,6 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class ErrorHandler(tornado.web.ErrorHandler, IndexHandler):
     pass
-
-
-class AuthHandler(tornado.web.RequestHandler):
-    def prepare(self):
-        self.token = self.get_argument('token')
-        result = db.users.find_one({'token': self.token})
-
-        if result is None:
-            raise tornado.web.HTTPError(400)
-
-
-
-class CreateAccount(IndexHandler):
-    def prepare(self):
-        self.token = self.get_argument('token')
-        posts = db.posts
-        result = posts.find_one({'token': self.token})
-        if not (result is None):
-            raise tornado.web.HTTPError(400)
-
-        try:
-            self.idinfo = client.verify_id_token(self.token, CLIENT_ID)
-            if self.idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-                raise crypt.AppIdentityError('Wrong issuer.')
-
-        except crypt.AppIdentityError as e:
-            raise tornado.web.HTTPError(400, reason=e)
-
-    def post(self):
-        email = self.idinfo['email']
-        name = self.idinfo['name']
-        post_data = {
-            'token': self.token,
-            'name': name,
-            'email': email
-        }
-        db.users.insert_one(post_data)
 
 
 class AllTypes(IndexHandler, AuthHandler):
@@ -113,7 +63,6 @@ if __name__ == "__main__":
             (r"/", AllTypes),
             (r"/hackathons", Hacks),
             (r"/conferences", Conf),
-            (r"/create", AuthHandler)
         ], **settings
     )
     http_server = tornado.httpserver.HTTPServer(app)
